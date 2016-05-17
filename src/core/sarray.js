9f71435340f8a6cb204762996f62614841354ef9
Dave Evans <dave@playcanvas.com>
2016-05-17 11:51:58 +0100

    comments

--- src/core/sarray.js
+++ src/core/sarray.js
@@ -1,15 +1,15 @@
 pc.extend(pc, function () {
     var SArray = function (length) {
         length = length || 1;
-        if (this.data) {
-            // this.data.length = length;
-        } else {
+        if (!this.data) {
+            // check for pre-existing data in-case this is being pooled
             this.data = new Array(length);
         }
         this.length = 0;
     }
 
     SArray.prototype = {
+        // copy other array into this array
         copy: function (arr) {
             // resize until there is room
             while (arr.length > this.data.length) {
@@ -23,6 +23,7 @@ pc.extend(pc, function () {
             this.length = arr.length;
         },
 
+        // set a value at index
         set: function (index, value) {
             if(index >= this.data.length) {
                 this.resize(this.data.length*2);
@@ -30,6 +31,7 @@ pc.extend(pc, function () {
             this.data[index] = value;
         },
 
+        // push new value onto end
         push: function (item) {
             if (this.length >= this.data.length) {
                 this.resize(this.data.length*2);
@@ -39,18 +41,20 @@ pc.extend(pc, function () {
             return this.length;
         },
 
+        // pop value off end
         pop: function () {
             if (this.length) {
                 return this.data[this.length--];
             }
         },
 
+        // resize
         resize: function (length) {
             console.log("resize from: " + this.data.length + " to " + length);
 
             var _data = this.data;
 
-            this.data = new Array(_data.length*2);
+            this.data = new Array(length*2);
             for (var i = 0; i < _data.length; i++) {
                 this.data[i] = _data[i];
             }
