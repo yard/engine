b69c4d2f0572f29bebc2868dac3d001e4bf5e59c
Anton Zhuravsky <mail2lf@gmail.com>
2017-01-07 16:59:54 +0300

    FIX: updating screens in children when it's added to a present hierarchy

--- src/framework/components/screen/system.js
+++ src/framework/components/screen/system.js
@@ -53,6 +53,8 @@ pc.extend(pc, function () {
             }
 
             ScreenComponentSystem._super.initializeComponentData.call(this, component, data, properties);
+
+            component._updateScreenInChildren();
         },
 
         _onUpdate: function (dt) {
